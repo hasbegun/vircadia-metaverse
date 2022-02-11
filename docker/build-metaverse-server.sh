887f2e8de5dde8c325864dcfc59666d29605786b
Inho Choi <inho.choi@menlosecurity.com>
2022-02-10 21:15:39 -0800

    generate key, cert and chain

--- docker/build-metaverse-server.sh
+++ docker/build-metaverse-server.sh
@@ -1,1 +1,13 @@
-docker build -t metaverse-server -f Dockerfile .
+#! /bin/bash
+
+CERT=files/cert.pem
+KEY=files/key.pem
+CHAIN=files/chain.pem
+
+if [ -f "$CERT" ] && [ -f "$KEY" ] && [ -f "$CHAIN" ]; then
+  echo "Cert files exist. Skip generation of key."
+else
+  ./keygen.sh
+fi
+
+# docker build -t metaverse-server -f Dockerfile .
