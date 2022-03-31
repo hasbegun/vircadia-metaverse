f3e0f46da98c2f449624be21d1c0db469141140b
Inho Choi <inho.choi@menlosecurity.com>
2022-03-31 00:35:16 -0700

    metaverse https done

--- src/Tools/Db.ts
+++ src/Tools/Db.ts
@@ -54,7 +54,6 @@ export async function setupDB(): Promise<void> {
             optionsSpec += `?authSource=${Config.database["db-authdb"]}`;
         };
         connectUrl = `mongodb://${userSpec}@${hostSpec}/${optionsSpec}`;
-        Logger.info(`setupDb: connecting to DB at: mongodb://${userSpec}@${hostSpec}/${optionsSpec}`);
         Logger.debug(`setupDb: connecting to DB at: mongodb://USER@${hostSpec}/${optionsSpec}`);
     };
 
